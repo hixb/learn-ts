ab5fdca9ace24938ba88c2aa666d35890f4a53dc
hixb <Hello16369@163.com>
2022-07-12 21:27:07 +0800

    feat: function overloading

--- 02-learn/01-funcOverloading/01-funcOverloading.ts
+++ 02-learn/01-funcOverloading/01-funcOverloading.ts
@@ -30,12 +30,17 @@ let message: Message[] = [
 ]
 
 // 不使用函数重载来实现
+// 函数结构不分明, 可复性/可维护性差
 function getMessage(value: number | MessageType): Message | undefined | Array<Message> {
   return typeof value === "number"
     ? message.find(item => value === item.id)
     : message.filter(item => value === item.type);
 }
+// ts 没有办法在运行之前根据传递的值来推导方法最终返回的数据的类型
+// 只可以根据方法定义的类型展现
+// const msg = getMessage(1);
 
-console.log(getMessage("image"));
+const msg = (<Message>getMessage(1)).sendMessage;
+console.log(msg);
 
 export {}
