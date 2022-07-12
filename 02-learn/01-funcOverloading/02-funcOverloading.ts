4215ad555bb2e2c0ad5a2522890ec55e13392487
hixb <Hello16369@163.com>
2022-07-12 22:00:43 +0800

    feat: function overloading sign

---
+++ 02-learn/01-funcOverloading/02-funcOverloading.ts
@@ -0,0 +1,48 @@
+type MessageType = "image" | "audio" | string; // 微信消息类型
+type Message = {
+  id: number;
+  type: MessageType;
+  sendMessage: string;
+}
+
+let message: Message[] = [
+  {
+    id: 1, type: "image", sendMessage: "hello, 你好啊"
+  },
+  {
+    id: 2, type: "audio", sendMessage: "hi, 你好啊"
+  },
+  {
+    id: 3, type: "audio", sendMessage: "你去那里"
+  },
+  {
+    id: 4, type: "image", sendMessage: "你呢"
+  },
+  {
+    id: 5, type: "image", sendMessage: "北京"
+  },
+  {
+    id: 6, type: "audio", sendMessage: "哪?"
+  },
+  {
+    id: 7, type: "image", sendMessage: "沙雕?"
+  },
+]
+
+function getMessage(id: number): Message; // 重载签名, 可以有多个
+function getMessage(msgType: MessageType): Message[]; // 重载签名
+
+// 实现签名函数, 只有实现签名才有函数体, 实现签名只能有一个
+function getMessage(payload: unknown): Message[] | Message | undefined {
+  return typeof payload === "number"
+    ? message.find(item => payload === item.id)
+    : message.filter(item => payload === item.type);
+}
+
+const msg = getMessage("image");
+// msg.forEach(item => {
+//   console.log("msg", item.sendMessage);
+// })
+console.log(msg);
+
+export {}
