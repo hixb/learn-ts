d55cd231f2348ef25970d3dd07ab1d491ee37eba
hixb <Hello16369@163.com>
2022-07-19 20:31:24 +0800

    feat: Prototype chain extends

--- 04-extendsInDepth/01-prototypeChainExtends/prototypeExtends.js
+++ 04-extendsInDepth/01-prototypeChainExtends/prototypeExtends.js
@@ -3,6 +3,11 @@ function Parent(name, age) {
   this.age = age;
 }
 
+Parent.prototype.friends = ["xiaozhang", "xiaoli"];
+Parent.prototype.eat = function () {
+  console.log(this.name + "吃法");
+}
+
 function Son(favor, sex) {
   this.favor = favor;
   this.sex = sex;
@@ -16,8 +21,14 @@ console.log("sonObj: ", sonObj);
 console.log("Son.prototype", Son.prototype);
 
 // 原型链继承
+// 继承带来的好处
+//    子类对象变量可以访问父类的实例属性
+//    子类对象变量可以访问父类原型对象空间中的属性和方法
 Son.prototype = new Parent("往往", 44);
 console.log("Son.prototype 原型链继承之后的指向", Son.prototype);
 
 const sonObj2 = new Son("打篮球", "男");
-console.log("sonObj2: ", sonObj2)
+console.log("sonObj2: ", sonObj2);
+console.log("sonObj2访问son类自身的favor属性[构造函数中this定义的对象属性]: ", sonObj2.favor);
+console.log("sonObj2访问son对象原型上name属性: ", sonObj2, name);
+console.log("sonObj2:访问friends属性 ", sonObj2.friends);
